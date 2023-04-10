0505ebe3854a940a9d902dbcda47746ab44a5127
weishunyu <2072738187@qq.com>
2023-04-10 14:47:56 +0800

    feat

--- ios/AwesomeTSProject/AppDelegate.h
+++ ios/AwesomeTSProject/AppDelegate.h
@@ -1,6 +1,15 @@
 #import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
-
-@interface AppDelegate : RCTAppDelegate
+#if __has_include(<RCTGetuiModule/RCTGetuiModule.h>)
+#import <RCTGetuiModule/RCTGetuiModule.h>
+#elif __has_include("RCTGetuiModule.h")
+#import "RCTGetuiModule.h"
+#elif __has_include(<GtSdkRN/RCTGetuiModule.h>)
+#import <GtSdkRN/RCTGetuiModule.h>
+#endif
+#define kGtAppId @"THlKAXqZwA8l79tz1lK9A5"
+#define kGtAppKey @"V74xmxgf0d8Xkg3IkHTIx1"
+#define kGtAppSecret @"LWHJFUJsWe5QpAgF9nsg48"
+@interface AppDelegate : RCTAppDelegate<UNUserNotificationCenterDelegate, GeTuiSdkDelegate>
 
 @end
